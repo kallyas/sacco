824bb9042f2e66714ea3aba22861485ca20297d5
kallyas <kallyasmedia@gmail.com>
2025-04-11 16:34:39 +0300

    Add transaction viewset with create functionality and role-based access control

--- sacco_backend/apps/transactions/views.py
+++ sacco_backend/apps/transactions/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.transactions.models import Transaction
+from apps.transactions.serializers import TransactionSerializer
+from apps.transactions.services.transaction_service import TransactionService
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.role.name in ['STAFF', 'ADMIN']:
+            return Transaction.objects.all()
+        return Transaction.objects.filter(member__user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            transaction = TransactionService.create_transaction(**serializer.validated_data)
+            return Response(
+                TransactionSerializer(transaction).data,
+                status=status.HTTP_201_CREATED
+            )
+        except ValueError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
