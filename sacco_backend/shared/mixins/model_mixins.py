fbc4df20418c33f3112233f9313392595a756f1e
kallyas <kallyasmedia@gmail.com>
2025-04-11 16:22:50 +0300

    Add model mixins for timestamping and soft deletion

--- sacco_backend/shared/mixins/model_mixins.py
+++ sacco_backend/shared/mixins/model_mixins.py
@@ -1,21 +1,44 @@
-# shared/mixins/audit_mixin.py
+# shared/mixins/model_mixins.py
+from django.conf import settings
 from django.db import models
 
+from apps import authentication
+
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 class AuditMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(
-        'apps.authentication.User',
+        'authentication.User',
         on_delete=models.SET_NULL,
         null=True,
         related_name='%(class)s_created'
     )
     updated_by = models.ForeignKey(
-        'apps.authentication.User',
+        'authentication.User',
         on_delete=models.SET_NULL,
         null=True,
         related_name='%(class)s_updated'
+    )
+
+    class Meta:
+        abstract = True
+
+class SoftDeleteMixin(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True)
+    deleted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='%(class)s_deleted'
     )
 
     class Meta:
