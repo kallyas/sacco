c95ccae3b6aedf65798f4066230db3d11e9ecab2
kallyas <kallyasmedia@gmail.com>
2025-04-11 16:32:52 +0300

    Add URL routing for authentication, integrations, loans, members, risk management, ledger, and reporting views

--- sacco_backend/core/urls.py
+++ sacco_backend/core/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.authentication.urls')),
+    path('api/v1/', include('apps.integrations.urls')),
+    path('api/v1/', include('apps.loans.urls')),
+    path('api/v1/', include('apps.members.urls')),
+    path('api/v1/', include('apps.risk_management.urls')),
+    path('api/v1/', include('apps.ledger.urls')),
+    path('api/v1/', include('apps.reporting.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
