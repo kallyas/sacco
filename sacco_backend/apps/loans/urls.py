2960ba402c650393ab1afe91ab15a5fed087c31f
kallyas <kallyasmedia@gmail.com>
2025-04-11 16:32:28 +0300

    Add URL routing for loan management views

---
+++ sacco_backend/apps/loans/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import LoanViewSet, LoanRepaymentViewSet, LoanApplicationViewSet
+
+router = DefaultRouter()
+
+router.register(r'loans', LoanViewSet, basename='loans')
+router.register(r'loan_repayments',LoanRepaymentViewSet, basename='loan_repayments')
+router.register(r'loan_applications', LoanApplicationViewSet, basename='loan_applications')
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
