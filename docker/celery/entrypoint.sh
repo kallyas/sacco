61102b86934abb39de5b6476524d65c920a4cdce
kallyas <kallyasmedia@gmail.com>
2025-04-15 16:12:51 +0300

    feat: Add entrypoint.sh to wait for database and brokers
    This commit introduces `docker/celery/entrypoint.sh`, an entrypoint script
    for the Celery worker container. The script ensures that PostgreSQL,
    RabbitMQ, and Redis are available before starting the Celery worker.
    
    It uses `postgres_ready`, `rabbitmq_ready`, and `redis_ready` functions
    to check the availability of each service by attempting to connect to them.
    The script waits and retries until each service is up.
    
    This prevents the Celery worker from starting prematurely and failing due
    to unavailable dependencies. Finally, it executes the command passed to
    the container via "$@".

---
+++ docker/celery/entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Wait for PostgreSQL
+postgres_ready() {
+python << END
+import sys
+import psycopg2
+try:
+    psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="${POSTGRES_HOST}",
+        port="${POSTGRES_PORT}",
+    )
+except psycopg2.OperationalError:
+    sys.exit(1)
+sys.exit(0)
+END
+}
+
+# Wait for RabbitMQ
+rabbitmq_ready() {
+  nc -z ${RABBITMQ_HOST} ${RABBITMQ_PORT}
+}
+
+# Wait for Redis
+redis_ready() {
+  nc -z ${REDIS_HOST} ${REDIS_PORT}
+}
+
+until postgres_ready; do
+  echo >&2 "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+echo >&2 "PostgreSQL is up - continuing..."
+
+until rabbitmq_ready; do
+  echo >&2 "RabbitMQ is unavailable - sleeping"
+  sleep 1
+done
+echo >&2 "RabbitMQ is up - continuing..."
+
+until redis_ready; do
+  echo >&2 "Redis is unavailable - sleeping"
+  sleep 1
+done
+echo >&2 "Redis is up - continuing..."
+
+exec "$@"
