f1aacb1f8dc7acc76fc20c5657df2b094ddfc018
kallyas <kallyasmedia@gmail.com>
2025-05-08 19:54:34 +0300

    feat: Update build configuration and add terser for minification

--- sacco_frontend/vite.config.ts
+++ sacco_frontend/vite.config.ts
@@ -21,6 +21,39 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Core React dependencies
+          'react-core': ['react', 'react-dom', 'react-router-dom'],
+          
+          // Form-related dependencies
+          'form-libs': ['react-hook-form', '@hookform/resolvers', 'yup'],
+          
+          // MUI components and related
+          'mui-core': ['@mui/material', '@emotion/react', '@emotion/styled'],
+          'mui-icons': ['@mui/icons-material'],
+          'mui-data': ['@mui/x-data-grid', '@mui/x-date-pickers'],
+          
+          // Utility libraries
+          'utils': ['axios', 'date-fns', 'js-cookie'],
+        },
+        // Ensure chunks have meaningful names
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+    // Additional optimizations
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    // Limit chunk size warnings
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     proxy: {
       "/api": {
